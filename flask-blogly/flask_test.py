0175bbf4e1bd12715c9c3e3b87561b4d7c037531
Ralph's Code <RalphsCode@gmail.com>
2024-06-19 14:38:06 -0700

    Updated the UnitTesting and added new tests.

--- flask-blogly/flask_test.py
+++ flask-blogly/flask_test.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 # Use test database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Ponderosa@localhost/blogly'
@@ -28,7 +28,9 @@ class UserViewsTestCase(TestCase):
         with app.app_context():
             User.query.delete()
             user = User(first_name="Testy", last_name="McTester", image_url='https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png')
+            post = Post(title="Test Post", content="A test post for unitTesting.", user_id="1")
             db.session.add(user)
+            db.session.add(post)
             db.session.commit()
             self.user_id = user.id
 
@@ -55,3 +57,10 @@ class UserViewsTestCase(TestCase):
             html = resp.get_data(as_text=True)
             self.assertEqual(resp.status_code, 200)
             self.assertIn("Update", html)
+
+    def test_post(self):
+        with app.test_client() as client:
+            resp = client.get("/posts/1")
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn("Test Post", html)
